9a9a4052b512f53ebf57c0915681d5df6dd244c1
hero-peng <pengyabin2008@163.com>
2014-11-04 22:38:18 +0800

    取消监听用户解锁消息

--- AlarmManager/src/com/coder80/timer/receiver/BootBroadcastReceiver.java
+++ AlarmManager/src/com/coder80/timer/receiver/BootBroadcastReceiver.java
@@ -19,8 +19,7 @@ public class BootBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         mContext = context;
-        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED) ||
-                intent.getAction().equals(Intent.ACTION_USER_PRESENT)) {
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
             Log.i("BootBroadcastReceiver", "BroadcastReceiver onReceive here.... ");
             Handler handler = new Handler(Looper.getMainLooper());
             //after reboot the device,about 2 minutes later,upload the POI info to server
